cf0786bd913cf22ddd1381dc00bde3a751e2b798
Jean Etchichury <jean.etchichury@hotmail.com>
2024-02-09 18:09:52 -0300

    Criando entidade paciente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PacienteModule } from './paciente/paciente.module';
 
 @Module({
-  imports: [],
+  imports: [PacienteModule],
   controllers: [AppController],
   providers: [AppService],
 })
